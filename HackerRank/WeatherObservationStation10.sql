c9112fc43e42c2cb52ab33a7e2a23bb6a4cc1f7c
Torsa Times <50965423+TorsaTimes@users.noreply.github.com>
2023-04-06 22:23:35 +0200

    Create WeatherObservationStation10.sql

---
+++ HackerRank/WeatherObservationStation10.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) IN ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z');
